cf0a0097a9be23c3f201e06dcc09396f10cd3c0f
Garrett Nickel <gmnicke2@illinois.edu>
2017-02-24 04:04:43 -0600

    added timout on index view rendering of 1s to (lazily) wait for api

--- controllers/index.js
+++ controllers/index.js
@@ -26,16 +26,16 @@ var grimySnapdragon = {'name': 'Grimy toadflax', 'id': 3051, 'price': 0};
 var guam = {'name': 'Guam leaf', 'id': 249, 'price': 0};
 var marrentill = {'name': 'Marrentill', 'id': 251, 'price': 0};
 var tarromin = {'name': 'Tarromin', 'id': 253, 'price': 0};
-var harralander = {'name': 'Guam leaf', 'id': 255, 'price': 0};
-var ranarr = {'name': 'Marrentill', 'id': 257, 'price': 0};
-var irit = {'name': 'Tarromin', 'id': 259, 'price': 0};
-var avantoe = {'name': 'Guam leaf', 'id': 261, 'price': 0};
-var kwuarm = {'name': 'Marrentill', 'id': 263, 'price': 0};
-var cadantine = {'name': 'Tarromin', 'id': 265, 'price': 0};
-var dwarf = {'name': 'Guam leaf', 'id': 267, 'price': 0};
-var torstol = {'name': 'Marrentill', 'id': 269, 'price': 0};
-var toadflax = {'name': 'Guam leaf', 'id': 2998, 'price': 0};
-var snapdragon = {'name': 'Marrentill', 'id': 3000, 'price': 0};
+var harralander = {'name': 'Harralander', 'id': 255, 'price': 0};
+var ranarr = {'name': 'Ranarr weed', 'id': 257, 'price': 0};
+var irit = {'name': 'Irit leaf', 'id': 259, 'price': 0};
+var avantoe = {'name': 'Avantoe', 'id': 261, 'price': 0};
+var kwuarm = {'name': 'Kwuarm', 'id': 263, 'price': 0};
+var cadantine = {'name': 'Cadantine', 'id': 265, 'price': 0};
+var dwarf = {'name': 'Dwarf weed', 'id': 267, 'price': 0};
+var torstol = {'name': 'Torstol', 'id': 269, 'price': 0};
+var toadflax = {'name': 'Toadflax', 'id': 2998, 'price': 0};
+var snapdragon = {'name': 'Snapdragon', 'id': 3000, 'price': 0};
 
 var grimyHerbs = [grimyGuam, grimyMarrentill, grimyTarromin, grimyHarralander,
                   grimyRannar, grimyIrit, grimyAvantoe, grimyKwuarm, grimyCadantine,
@@ -48,34 +48,30 @@ var cleanHerbs = [guam, marrentill, tarromin, harralander,
 // Register method to GET from RSBuddy API
 client.registerMethod('callAPI', rsbuddyAPIURL, 'GET');
 
-var getPrice = (item, cb) => {
-  console.log('Getting price for %s', item['name']);
+var getPrice = (item) => {
+  console.log('Getting price for %s.', item['name']);
 
   var args = {
       parameters: {a: 'guidePrice', i: item['id']} // GET request params
   };
 
   client.methods.callAPI(args, (data, res) => {
-
     item['price'] = item['name'].includes('Grimy') ? data['buying'] : data['selling'];
-
-    cb();
+    console.log('got '+item['name']+'\'s price.');
   });
-};
-
-var updateRender = () => {
-  // TODO update prices in view when getPrice calls as callback
 };
 
 exports.index = (req, res) => {
-  for(var i = 0; i < grimyHerbs.length; i++) {
-    getPrice(grimyHerbs[i], updateRender);
-    getPrice(cleanHerbs[i], updateRender);
-  }
+  grimyHerbs.forEach(getPrice);
+  cleanHerbs.forEach(getPrice);
 
-  res.render('index', {
-    title: 'Herb Cleaning Profits (07scape)',
-    grimyGuamPrice: grimyGuam['price'],
-    guamPrice: guam['price']
-  });
+  setTimeout(() => {
+    res.render(
+      'index', 
+      {
+        title: 'Herb Cleaning Profits (07scape)',
+        grimyGuamPrice: grimyGuam['price'],
+        guamPrice: guam['price']
+      })
+    }, 1000);
 };
